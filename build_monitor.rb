7d1b63bf26c3e466e9d087515a243290542bce40
Pip Taylor <pip@evilgeek.co.uk>
2013-02-06 11:49:44 +0000

    [+AD] Add broken build list, modify styling.

--- build_monitor.rb
+++ build_monitor.rb
@@ -1,8 +1,27 @@
 require 'bundler/setup'
 require 'sinatra'
 require "sinatra/reloader" if development?
+require 'open-uri'
 
 get '/' do
   @display_time = Time.now.strftime("%H:%M")
+  @broken_builds = CI.new.broken_builds
   haml :index
+end
+
+class CI
+  def initialize(url="http://ci/api/json")
+    @url = url
+  end
+
+  def broken_builds
+    status["jobs"].select{|job| job["color"] == 'red' }
+  end
+
+private
+  attr_reader :url
+
+  def status
+    JSON.parse(open(url).read)
+  end
 end
